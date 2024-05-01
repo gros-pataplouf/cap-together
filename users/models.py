05e9aa18828bbb117361ddd61beaf6b3ff317ee5
gros-pataplouf <sceres@pm.me>
2024-05-01 11:13:41 +0200

    initial: scaffold django app without vite integration

---
+++ users/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = BaseUserManager()
+
+    def __str__(self):
+        return self.email
