8c8de261dfc808116c7bbf7872f9f8a237b0a680
gros-pataplouf <sceres@pm.me>
2024-05-02 15:26:59 +0200

    chore: basic setup, still need to decide what to do with static files

---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+ReactDOM.createRoot(document.getElementById('app2')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
