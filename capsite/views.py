b4f749050a299f15b54e72cf2f31f5d522f94ecd
gros-pataplouf <sceres@pm.me>
2024-05-03 12:48:19 +0200

    set up redirect to home

--- capsite/views.py
+++ capsite/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 
-
-# Create your views here.
-def index(request):
-    return render(request, "capsite/home_page.html")
+def redirect_view(request):
+    response = redirect('/home')
+    return response
--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from capsite import urls as capsite_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -26,5 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cms/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
+    path('', include(capsite_urls)),
     path('', include(wagtail_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
