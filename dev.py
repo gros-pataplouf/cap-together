8fb58b9e2b38f7bb8dbe5f8d39e2c4796a55674b
gros-pataplouf <sceres@pm.me>
2024-05-04 09:52:12 +0200

    add script for development

---
+++ dev.py
@@ -0,0 +1,12 @@
+import os
+
+#has to be run within an activated virtual environment
+
+def main():
+    os.system('gnome-terminal -- zsh -c "python manage.py runserver; exec zsh"')
+    os.system('gnome-terminal -- zsh -c "cd js_css && npm run dev; exec zsh"')
+    os.system('gnome-terminal -- zsh -c "cd js_css && npx tailwindcss -i ./src/input.css -o ./src/index.css --watch"')
+
+
+if __name__ == "__main__":
+    main()
