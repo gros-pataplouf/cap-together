b7f939a036a613705755478e17d735af1a690356
gros-pataplouf <sceres@pm.me>
2024-07-06 23:48:34 +0200

    correct flake8 errors

--- capsite/tests.py
+++ capsite/tests.py
@@ -1,38 +1,35 @@
-import unittest, django, os
+import unittest
+import django
+import os
 from wagtail.test.utils import WagtailPageTestCase
 from capsite.models import HomePage
-from core.settings import dev as settings
 
 env = os.environ.copy()
 DJANGO_SETTINGS_MODULE = env['DJANGO_SETTINGS_MODULE']
 django.setup()
 
+
 class HomePageTests(WagtailPageTestCase):
     def test_can_create_page(self):
         new_page = HomePage(
-            hero_text = "This is a test.",
-            path = 'test',
-            depth = 0,
-            title = 'Test Page',
-            slug = 'testing'
-
+            hero_text="This is a test.",
+            path='test',
+            depth=0,
+            title='Test Page',
+            slug='testing'
         )
         new_page.save()
-    
+
     def test_can_navigate_to_page(self):
         new_page = HomePage(
-            hero_text = "This is a test.",
-            path = 'test',
-            depth = 0,
-            title = 'Test Page',
-            slug = 'testing'
-
+            hero_text="This is a test.",
+            path='test',
+            depth=0,
+            title='Test Page',
+            slug='testing'
         )
         new_page.save()
         self.assertPageIsEditable(new_page)
-
-
-    
 
 
 if __name__ == '__main__':
