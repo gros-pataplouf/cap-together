d120d43f759647f4286768ffa73c8309bcac35a8
gros-pataplouf <sceres@pm.me>
2024-07-07 18:44:49 +0200

    #major: add healthcheck

--- capsite/views.py
+++ capsite/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import redirect
+from django.http import HttpResponse
 
 
 def redirect_view(request):
     response = redirect('/home')
     return response
+
+
+def healthcheck(request):
+    return HttpResponse("Healthcheck successful")
